570b735dec9809fd8bd2832df36307c4b1a4e03a
Bander IMAC <bander.jumah@gmail.com>
2020-08-29 22:46:40 +0300

    button

--- lib/app/signin/sign_in_page.dart
+++ lib/app/signin/sign_in_page.dart
@@ -26,18 +26,11 @@ class SignInPage extends StatelessWidget {
             style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w400),
           ),
           SizedBox(height: 8.0),
-          Container(
-            color: Colors.blue,
-            child: SizedBox(
-              height: 100,
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Container(
-            color: Colors.green,
-            child: SizedBox(
-              height: 100,
-            ),
+          RaisedButton(
+            child: Text('Sign in with google'),
+            onPressed: () {
+              print('Button Pressed');
+            },
           ),
         ],
       ),
